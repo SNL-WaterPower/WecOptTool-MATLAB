32aa8947a0c5c233f0088621416419fa299a831d
Mathew Topper <damm_horse@yahoo.co.uk>
2020-03-18 14:00:46 +0000

    Encapsulate the Device Model (#52)
    * Adds the DeviceModelTemplate abstract class for encapsulating methods to calculate the hydrodynamics and forces parameters for the control calculations of the generic device. The concrete implementation for RM3 is provided in the RM3DeviceModel class.
    
    * Add iteration over sea-states. There are some notes on improvements which need to be made for error checking on passed arguments etc in the code.
    
    * Simplify maxvalues check for PS
    
    * Set mu=1 when only a single spectra is passed.
    
    * The package currently states that the spectra standard for the WecOptTool is defined as that returned by WAFO which is of the size (N,1). The function subSampleFreqs() was returning spectra of size (1,N). This was corrected. Further the extrapolation method was specified to insert 0 vals during interpolation instead of defining NaNs as 0 following interpolation.
    
    * Moved subSampleFreqs() from units tests to volatile as its own function. This function is intended to reduce the computational load e.g. reduce the number of frequencies analyzed.
    
    * Corrected the building of struct RM3 such that variables are of size Nx1
    
    * Renamed RM_eval() to buildRM3 to better reflect the function purpose.
    
    * Identify location of needed spectraCheck function.
    
    Co-authored-by: ssolson <ssolson@sandia.gov>

--- toolbox/+WecOptLib/+tests/+unit/RM3_unitTest.m
+++ toolbox/+WecOptLib/+tests/+unit/RM3_unitTest.m
@@ -17,8 +17,8 @@ end
 function testVerify_CC(testCase)
 S = WecOptLib.tests.data.exampleSpectrum();
 S.ph = rand(length(S.w),1)* 2 * pi;
-[S.w, S.S] = subSampleFreqs(S);
-WECpow = WecOptLib.volatile.RM3_getPow(S,'CC','scalar',1);
+RM3Device = WecOptLib.volatile.RM3DeviceModel();
+WECpow = RM3Device.getPower(S,'CC','scalar',1);
 expSol = -3.772016088262561e+06;
 verifyEqual(testCase, WECpow, expSol, 'RelTol', 0.001)
 end
@@ -26,19 +26,32 @@ end
 function testVerify_damping(testCase)
 S = WecOptLib.tests.data.exampleSpectrum();
 S.ph = rand(length(S.w),1)* 2 * pi;
-[S.w, S.S] = subSampleFreqs(S);
-WECpow = WecOptLib.volatile.RM3_getPow(S,'P','scalar',1);
+[S.w, S.S] = WecOptLib.volatile.subSampleFreqs(S);
+RM3Device = WecOptLib.volatile.RM3DeviceModel();
+WECpow = RM3Device.getPower(S,'P','scalar',1);
 expSol = -1.349990052717686e+06;
 verifyEqual(testCase, WECpow, expSol, 'RelTol', 0.001)
 end
 
+% function testVerify_SeaStates(testCase)
+% % Load Sea States
+% SS = load('Y:\WecOptTool\toolbox\+WecOptLib\+tests\+data\sea-states.mat');
+% 
+% %S.ph = rand(length(S.w),1)* 2 * pi;
+% %[S.w, S.S] = WecOptLib.volatile.subSampleFreqs(S);
+% WECpow = WecOptLib.volatile.SeaStatePower(S,'P','scalar',1);
+% expSol = -1.349990052717686e+06;
+% verifyEqual(testCase, WECpow, expSol, 'RelTol', 0.001)
+% end
+
 function testVerify_PS(testCase)
 S = WecOptLib.tests.data.exampleSpectrum();
 S.ph = rand(length(S.w),1)* 2 * pi;
-[S.w, S.S] = subSampleFreqs(S);
+[S.w, S.S] = WecOptLib.volatile.subSampleFreqs(S);
 delta_Zmax = 10;
 delta_Fmax = 1e9;
-WECpow = WecOptLib.volatile.RM3_getPow(S,'PS','scalar',1,[delta_Zmax,delta_Fmax]);
+RM3Device = WecOptLib.volatile.RM3DeviceModel();
+WECpow = RM3Device.getPower(S,'PS','scalar',1,[delta_Zmax,delta_Fmax]);
 expSol = -3.772016088252104e+06;
 verifyEqual(testCase, WECpow, expSol, 'RelTol', 0.001)
 end
@@ -54,39 +67,20 @@ function test_existingRunFiles(testCase)
 tol = 5 * eps;
 S = WecOptLib.tests.data.exampleSpectrum();
 S.ph = rand(length(S.w),1)* 2 * pi;
-[S.w, S.S] = subSampleFreqs(S);
-[madepow,etc] = WecOptLib.volatile.RM3_getPow(S,'CC','parametric',[10,15,3,42]);
+[S.w, S.S] = WecOptLib.volatile.subSampleFreqs(S);
+RM3Device = WecOptLib.volatile.RM3DeviceModel();
+[madepow,etc] = RM3Device.getPower(S,'CC','parametric',[10,15,3,42]);
 madeFile = etc.rundir;
-[existpow,~] = WecOptLib.volatile.RM3_getPow(S,'CC','existing', madeFile);
+[existpow,~] = RM3Device.getPower(S,'CC','existing', madeFile);
 verifyEqual(testCase, madepow, existpow, 'RelTol', tol);
 end
 
 function test_runParametric(testCase)
 S = WecOptLib.tests.data.exampleSpectrum();
 S.ph = rand(length(S.w),1)* 2 * pi;
-[S.w, S.S] = subSampleFreqs(S);
-WECpow = WecOptLib.volatile.RM3_getPow(S,'CC','parametric',[10,15,3,42]);
+[S.w, S.S] = WecOptLib.volatile.subSampleFreqs(S);
+RM3Device = WecOptLib.volatile.RM3DeviceModel();
+WECpow = RM3Device.getPower(S,'CC','parametric',[10,15,3,42]);
 expSol = -4.415667556078834e+06;
 verifyEqual(testCase, WECpow, expSol, 'RelTol', 0.001)
-end
-
-function [new_w, new_S] = subSampleFreqs(S, npoints)
-%subSampleFreq - subsamples sea state and interpolates to three harmonics
-%    gets a subsampling of a given seastate by linear interpolation
-%    Inputs:
-%        S = seastate.  must have S.S and S.w
-%        npoints = number of points to subsample
-%    Outputs:
-%        newS = new density values
-%        neww = new frequency values
-
-if(nargin < 2)
-    npoints = 120;
-end
-ind_sp = find(S.S > 0.01 * max(S.S),1,'last');
-
-new_w = linspace(S.w(1), S.w(ind_sp) * 3, npoints);
-new_S = interp1(S.w, S.S, new_w);
-
-new_S(isnan(new_S)) = 0;
 end
---
+++ toolbox/+WecOptLib/+volatile/buildRM3.m
@@ -0,0 +1,113 @@
+function RM3 = buildRM3(S, hydro, controlType, maxVals)
+% pow_ss = buildRM3(S, hydro, controlType, geomMode, maxVals)
+%
+% Returns one Sea-state power given spectra, BEM response, geomMode,
+%    and the max vals
+%
+% Inputs
+%       S               wave spectra structure, with fields:
+%           S.S         spectral energy distribution [m^2 s/rad]
+%           S.w         frequency [rad/s]
+%           S.ph        phasing
+%           S.mu        weighting factor
+%       hydro           hydro struct from Nemoh
+%       controlType     choose control type for PTO:
+%                           CC: complex conjugate (no constraints)
+%                           PS: pseudo-spectral (with constraints)
+%                           P: proportional damping
+%       geomMode        choose mode for geometry definition; geometry
+%                       inputs are passed in as string-value pairs, see
+%                       RM3_getNemoh documentation for more info.
+%                           scalar: single scaling factor lambda
+%                           parametric: [r1, r2, d1, d2] parameters
+%                           existing: pass existing NEMOH rundir
+%       controlParams   if using the 'PS' control type, optional arguments 
+%                       for deltaZmax and deltaFmax
+%                           Note: to use the optional arguments, both must
+%                           be provided
+% Outputs
+%       pow_ss           power weighted by weighting factors
+%
+% Examples:
+% 1) Using scalar input with CC control and scaling factor 1:
+%  WecOptLib.volatile.buildRM3(S, hydro, controlType, maxVals);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Set max values for PS control
+if strcmp(controlType, 'PS') && length(maxVals) == 2
+    delta_Zmax = maxVals(1);
+    delta_Fmax = maxVals(2);
+    RM3.delta_Zmax = delta_Zmax;
+    RM3.delta_Fmax = delta_Fmax;
+end
+
+% Ignore tails of the spectra; return indicies of the vals>1% of max
+iSpec = find(S.S > 0.01*max(S.S));
+% Return column vector of all w between first/last indicies
+iStart = min(iSpec);
+iEnd   = max(iSpec);
+iSkip  = 1;
+w = S.w(iStart:iSkip:iEnd);
+% Calculate w step-size
+if length(iSpec) == 1
+    dw = wStep;    
+else    
+    dw = mean(diff(S.w))*iSkip;   
+end
+
+% Get column vector S at same indicies as w (Removed interpolation). 
+s = S.S(iStart:iSkip:iEnd);
+% TODO: is interp needed? %s = interp1(S.w(:), S.S, w,'linear',0);
+% Calculate wave amplitude
+waveAmp = sqrt(2 * dw * s);
+% Row vector of random phases?
+ph = rand(length(s), 1);
+eta_fd = waveAmp .* exp(1i*ph);
+eta_fd = eta_fd(:);
+
+% Save the frequency
+RM3.w = w;
+RM3.dw = dw;
+% Only for PS
+RM3.wave_amp = waveAmp;
+RM3.ph = ph;
+
+% mass
+RM3.mass1 = hydro.Vo(1) * hydro.rho;
+RM3.mass2 = hydro.Vo(2) * hydro.rho;
+
+% restoring
+RM3.K3 = hydro.C(3,3,1) * hydro.g * hydro.rho;
+RM3.K9 = hydro.C(3,3,2) * hydro.g * hydro.rho;
+
+% radiation
+RM3.B99 = interp1(hydro.w,squeeze(hydro.B(9, 9, :)),w,'linear',0) * hydro.rho .* w;
+RM3.A99 = interp1(hydro.w,squeeze(hydro.A(9, 9, :)),w,'linear',0) * hydro.rho;
+
+RM3.B39 = ((interp1(hydro.w,squeeze(hydro.B(3, 9, :)),w,'linear',0) + ...
+    interp1(hydro.w,squeeze(hydro.B(9, 3, :)),w,'linear',0)) ./ 2) * hydro.rho .* w;
+RM3.A39 = ((interp1(hydro.w,squeeze(hydro.A(3, 9, :)),w,'linear',0) + ...
+    interp1(hydro.w,squeeze(hydro.A(9, 3, :)),w,'linear',0)) ./ 2) * hydro.rho;
+
+RM3.B33 = squeeze(interp1(hydro.w,squeeze(hydro.B(3, 3, :)),w,'linear',0)) * hydro.rho .* w;
+RM3.A33 = squeeze(interp1(hydro.w,squeeze(hydro.B(3, 3, :)),w,'linear',0)) * hydro.rho;
+
+% excitation
+RM3.H3 = interp1(hydro.w,complex(squeeze(hydro.ex_re(3, 1, :)), ...
+    squeeze(hydro.ex_im(3, 1, :))),w,'linear',0)...
+    * hydro.g * hydro.rho;
+RM3.H9 = interp1(hydro.w,complex(squeeze(hydro.ex_re(9, 1, :)), ...
+    squeeze(hydro.ex_im(9, 1, :))),w,'linear',0)...
+    * hydro.g * hydro.rho;
+
+% friction
+RM3.Bf = max(RM3.B33) * 0.1;
+
+RM3.D3 = 0;
+RM3.D9 = 0;
+
+RM3.E3 = RM3.H3 .* eta_fd;
+RM3.E9 = RM3.H9 .* eta_fd;
+
+end
