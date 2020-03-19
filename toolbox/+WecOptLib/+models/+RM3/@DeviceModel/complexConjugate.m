4560b6f2c70185c49c34118182d53268d40cb5e4
ssolson <ssolson@sandia.gov>
2020-03-19 08:57:56 -0600

    Code clean up, comments. Change to output totalpower  and power per frequency to match PS output.

--- toolbox/+WecOptLib/+models/+RM3/@DeviceModel/complexConjugate.m
+++ toolbox/+WecOptLib/+models/+RM3/@DeviceModel/complexConjugate.m
@@ -1,14 +1,23 @@
-function [pow_ss] = complexConjugate(obj, RM3)
+function [powSS, Pabs] = complexConjugate(obj, RM3)
+    % Returns total power and power per frequency
+
+    % Calculate Impedance
     Z3 = RM3.B33 + RM3.D3 + RM3.Bf + 1i*(RM3.w.*(RM3.mass1 + RM3.A33) - RM3.K3./RM3.w);
     Z9 = RM3.B99 + RM3.D9 + RM3.Bf + 1i*(RM3.w.*(RM3.mass2 + RM3.A99) - RM3.K9./RM3.w);
     Zc = RM3.B39 + 1i * RM3.w .* RM3.A39;
-
 
     Z0 = Z3 + Z9 + 2*Zc;
     Zi = (Z3.*Z9 - Zc.^2) ./ Z0;
 
     F0 = (RM3.E3.*(Z9+Zc) - RM3.E9.*(Z3+Zc))./Z0;
+    
+    % Power per frequency
     Pabs = abs(F0).^2 ./ (8* real(Zi));
+    
+    % Sea-state Power Total
+    powSS = -1 * sum(Pabs);
+    
+    %TODO: Determine if this code is useful going forward
 %     Ur = F0 ./ (2 * real(Zi));
 %     Sr = -1i * Ur ./ RM3.w;
 %     Fl = conj(Zi) .* Ur;
@@ -20,5 +29,4 @@ function [pow_ss] = complexConjugate(obj, RM3)
 %     urt = real(Ur .' * exp_mat);
 %     Flt = real(Fl .' * exp_mat);
 
-    pow_ss = -1 * sum(Pabs);
 end
