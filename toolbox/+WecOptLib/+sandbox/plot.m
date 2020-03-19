7800d5ba099785d37961b9976a814a82bce0a11f
Mathew Topper <damm_horse@yahoo.co.uk>
2020-03-19 12:23:44 +0000

    Add Plot Function for Investigation
    This is a play function for testing plotting with the pseudo spectral
    controller. It is called as WecOptLib.sandbox.plot()

---
+++ toolbox/+WecOptLib/+sandbox/plot.m
@@ -0,0 +1,29 @@
+function plot
+    %PLOT
+    
+    import WecOptLib.models.RM3.*
+    
+    S = WecOptLib.tests.data.exampleSpectrum();
+    S.ph = rand(length(S.w),1)* 2 * pi;
+    [S.w, S.S] = WecOptLib.utils.subSampleFreqs(S);
+    delta_Zmax = 10;
+    delta_Fmax = 1e9;
+    
+    RM3Device = DeviceModel();
+    
+    [hydro, ~] = RM3Device.getHydrodynamics('scalar', 1.);
+    
+    motion = RM3Device.getMotion(S,                         ...
+                                 hydro,                     ...
+                                 'PS',                      ...
+                                 [delta_Zmax, delta_Fmax]);
+    
+    motion = getPSCoefficients(motion);
+    ph = 5.3208;
+    [~, P] = getPSPhasePower(motion, ph);
+    
+    figure
+    plot(motion.W, P)
+    
+end
+    
