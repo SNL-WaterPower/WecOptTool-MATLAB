477aeec96e36b06a9ed36e2e4f974f8f1073c1d5
Mathew Topper <damm_horse@yahoo.co.uk>
2020-03-26 13:59:04 +0000

    Add license headers

--- toolbox/+WecOptLib/+tests/+unit/getNemohTest.m
+++ toolbox/+WecOptLib/+tests/+unit/getNemohTest.m
@@ -1,5 +1,22 @@
-% Setting up test function for getNemoh.
-% Following MATLAB suggested Function-Based Unit Tests Framework.
+
+% Copyright 2020 Sandia National Labs
+%
+% This file is part of WecOptTool.
+% 
+%     WecOptTool is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     WecOptTool is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+
+% Notes: 
 %
 % Currently testing cylinder and sphere shaped geometries, cube to come.
 %
@@ -14,11 +31,9 @@
 %
 % -Zachary Morrell 5/17/2019
 
-
 function tests = getNemohTest
    tests = functiontests(localfunctions);
 end
-
 
 function testCylinderM(testCase)
     tol = .001;
