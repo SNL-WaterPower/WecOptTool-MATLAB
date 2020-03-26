e10589fe8adcc79b120e5a96fc198dccb5d262c5
Mathew Topper <damm_horse@yahoo.co.uk>
2020-03-26 17:26:31 +0000

    Add leading slash to srcRootPath for linux

--- toolbox/+WecOptLib/+utils/getSrcRootPath.m
+++ toolbox/+WecOptLib/+utils/getSrcRootPath.m
@@ -4,5 +4,11 @@ function [srcRootPath] = getSrcRootPath()
     [pDir, ~, ~] = fileparts(p);
     parts = strsplit(pDir, filesep);
     srcRootPath = fullfile(parts{1:end-3});
+    
+    % Linux requires a leading slash
+    if ~ispc
+        srcRootPath = append(filesep, srcRootPath);
+    end
+    
 end
 
