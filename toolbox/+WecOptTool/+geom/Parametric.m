f491444c33ae1c77eda257d9d2c11bdb6ca3964f
Mathew Topper <damm_horse@yahoo.co.uk>
2020-04-28 19:51:16 +0100

    Add default for freqStep to docstring

--- toolbox/+WecOptTool/+geom/Parametric.m
+++ toolbox/+WecOptTool/+geom/Parametric.m
@@ -39,7 +39,8 @@ classdef Parametric < WecOptTool.geom.AbsGeom
     % The following options are supported:
     %
     %   freqStep (float):
-    %     Frequency discretisation step used in NEMOH calculations
+    %     Frequency discretisation step used in NEMOH calculations. Defaults to
+    %     0.2 (Hz).
     %
     
     properties
