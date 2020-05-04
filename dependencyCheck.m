793a93a1d48f61a319a2a1ad5e26aa464375c66f
Ryan Coe <rcoe@sandia.gov>
2020-05-04 12:44:00 -0600

    update indentations

--- dependencyCheck.m
+++ dependencyCheck.m
@@ -1,20 +1,20 @@
 
-% Copyright 2020 National Technology & Engineering Solutions of Sandia, 
-% LLC (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the 
+% Copyright 2020 National Technology & Engineering Solutions of Sandia,
+% LLC (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the
 % U.S. Government retains certain rights in this software.
 %
 % This file is part of WecOptTool.
-% 
+%
 %     WecOptTool is free software: you can redistribute it and/or modify
 %     it under the terms of the GNU General Public License as published by
 %     the Free Software Foundation, either version 3 of the License, or
 %     (at your option) any later version.
-% 
+%
 %     WecOptTool is distributed in the hope that it will be useful,
 %     but WITHOUT ANY WARRANTY; without even the implied warranty of
 %     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 %     GNU General Public License for more details.
-% 
+%
 %     You should have received a copy of the GNU General Public License
 %     along with WecOptTool.  If not, see <https://www.gnu.org/licenses/>.
 
@@ -89,8 +89,8 @@ fprintf('--------\n');
 %% Parallel Computing Toolbox
 
 [parallelToolboxFound,      ...
- parallelToolboxLicensed,   ...
- parallelToolboxInstalled] = WecOptLib.utils.hasParallelToolbox();
+    parallelToolboxLicensed,   ...
+    parallelToolboxInstalled] = WecOptLib.utils.hasParallelToolbox();
 
 if parallelToolboxFound
     fprintf('Parallel Toolbox:              Found\n');
