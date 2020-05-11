4c1727231efc6f559b5862e0a9eb5f5610f33d95
Ryan Coe <rcoe@sandia.gov>
2020-05-11 11:27:26 -0600

    update docs

--- toolbox/+WecOptLib/+models/@waveBot/waveBot.m
+++ toolbox/+WecOptLib/+models/@waveBot/waveBot.m
@@ -24,18 +24,19 @@ classdef waveBot < handle
     %
     % This file is part of WecOptTool.
     %
-    %     WecOptTool is free software: you can redistribute it and/or modify
-    %     it under the terms of the GNU General Public License as published by
-    %     the Free Software Foundation, either version 3 of the License, or
-    %     (at your option) any later version.
+    %     WecOptTool is free software: you can redistribute it and/or
+    %     modify it under the terms of the GNU General Public License as
+    %     published by the Free Software Foundation, either version 3 of
+    %     the License, or (at your option) any later version.
     %
-    %     WecOptTool is distributed in the hope that it will be useful,
-    %     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    %     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    %     GNU General Public License for more details.
+    %     WecOptTool is distributed in the hope that it will be useful, but
+    %     WITHOUT ANY WARRANTY; without even the implied warranty of
+    %     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    %     General Public License for more details.
     %
     %     You should have received a copy of the GNU General Public License
-    %     along with WecOptTool.  If not, see <https://www.gnu.org/licenses/>.
+    %     along with WecOptTool.  If not, see
+    %     <https://www.gnu.org/licenses/>.
     
     properties
         controlType     % 'P', 'CC', or 'PS'
@@ -63,6 +64,7 @@ classdef waveBot < handle
             %                                   radius (r1), radius at keel
             %                                   (r2), draft of chine (d1),
             %                                   and overall draft (d2)
+            %                   'existing'      TODO
             %   w               frequency vector [rad/s]
             
             if nargin < 3
