7ae3660c57eab77d7f5bb458916c83bea81fa4ea
ssolson <ssolson@sandia.gov>
2020-05-18 12:36:47 -0600

    Added a utility to get the mean dw from a spectra

---
+++ toolbox/+WecOptLib/+utils/getMeanDw.m
@@ -0,0 +1,39 @@
+function S = getMeanDw(S)
+    % getMeanDw - Returns the mean of the frequency discrtization for each w as
+    % a key in the struct S
+    %
+    % Parameters
+    % ----------
+    % S: struct
+    %     seastate must have S.w and S.S
+    %
+    % Returns
+    % -------
+    % S: struct
+    %     S with dw key as the mean difference 
+
+    % Copyright 2020 National Technology & Engineering Solutions of Sandia, 
+    % LLC (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the 
+    % U.S. Government retains certain rights in this software.
+    %
+    % This file is part of WecOptTool.
+    % 
+    %     WecOptTool is free software: you can redistribute it and/or modify
+    %     it under the terms of the GNU General Public License as published by
+    %     the Free Software Foundation, either version 3 of the License, or
+    %     (at your option) any later version.
+    % 
+    %     WecOptTool is distributed in the hope that it will be useful,
+    %     but WITHOUT ANY WARRANTY; without even the implied warranty of
+    %     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    %     GNU General Public License for more details.
+    % 
+    %     You should have received a copy of the GNU General Public License
+    %     along with WecOptTool.  If not, see <https://www.gnu.org/licenses/>.
+
+    WecOptLib.utils.checkSpectrum(S);
+
+    for i=1:length(S)    
+        S(i).dw =mean(diff(S(i).w));
+    end
+end
