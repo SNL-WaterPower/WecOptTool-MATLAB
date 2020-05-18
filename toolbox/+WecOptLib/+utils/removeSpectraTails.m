b9c360a2ec7d6f81d03229e613464a211e2ea9ae
ssolson <ssolson@sandia.gov>
2020-05-18 12:33:51 -0600

    Moved error checking and plotting out of functions.

--- toolbox/+WecOptLib/+utils/removeSpectraTails.m
+++ toolbox/+WecOptLib/+utils/removeSpectraTails.m
@@ -1,23 +1,3 @@
-
-% Copyright 2020 National Technology & Engineering Solutions of Sandia, 
-% LLC (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the 
-% U.S. Government retains certain rights in this software.
-%
-% This file is part of WecOptTool.
-% 
-%     WecOptTool is free software: you can redistribute it and/or modify
-%     it under the terms of the GNU General Public License as published by
-%     the Free Software Foundation, either version 3 of the License, or
-%     (at your option) any later version.
-% 
-%     WecOptTool is distributed in the hope that it will be useful,
-%     but WITHOUT ANY WARRANTY; without even the implied warranty of
-%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%     GNU General Public License for more details.
-% 
-%     You should have received a copy of the GNU General Public License
-%     along with WecOptTool.  If not, see <https://www.gnu.org/licenses/>.
-
 function noTailsSS = removeSpectraTails(SS, tailTolerence, minBins)
     % Removes the spectrum tails based on the provided tailTolerence such
     % that S > max(S)*tailTolerence/100 & length(S) >= minBins
@@ -41,16 +21,35 @@ function noTailsSS = removeSpectraTails(SS, tailTolerence, minBins)
     % noTailsSS: struct    
     %    spectra greater than the specified tailTolerence
 
+    % Copyright 2020 National Technology & Engineering Solutions of Sandia, 
+    % LLC (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the 
+    % U.S. Government retains certain rights in this software.
+    %
+    % This file is part of WecOptTool.
+    % 
+    %     WecOptTool is free software: you can redistribute it and/or modify
+    %     it under the terms of the GNU General Public License as published by
+    %     the Free Software Foundation, either version 3 of the License, or
+    %     (at your option) any later version.
+    % 
+    %     WecOptTool is distributed in the hope that it will be useful,
+    %     but WITHOUT ANY WARRANTY; without even the implied warranty of
+    %     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    %     GNU General Public License for more details.
+    % 
+    %     You should have received a copy of the GNU General Public License
+    %     along with WecOptTool.  If not, see <https://www.gnu.org/licenses/>.    
+    
     %TEMPORARY HARDCODED PLOT FUNCTION 
     plot=false;
     
     WecOptLib.utils.checkSpectrum(SS);
-    assertLengthOneOrLengthSS(tailTolerence,SS)
-    assertPositiveFloat(tailTolerence)
-    assertLengthOneOrLengthSS(minBins,SS)
-    assertPositiveInt(minBins)
-    assertMinBinsGreaterThanOne(minBins)
-    assertBinsGreaterThanMinBins(SS,minBins)    
+    WecOptLib.errorCheck.assertLengthOneOrLengthSS(tailTolerence,SS)
+    WecOptLib.errorCheck.assertPositiveFloat(tailTolerence)
+    WecOptLib.errorCheck.assertLengthOneOrLengthSS(minBins,SS)
+    WecOptLib.errorCheck.assertPositiveInteger(minBins)
+    WecOptLib.errorCheck.assertGreaterThan(minBins,1)
+    WecOptLib.errorCheck.assertStructFieldLengthGreaterThanOrEqualToReference(SS,'S',minBins)    
                       
     noTailsSS = SS;
     for i =1:length(SS)        
@@ -101,7 +100,7 @@ function [noTailW, noTailS] = removeTails(w, S, tailTolerence)
     SS.S = S;
     WecOptLib.utils.checkSpectrum(SS);
     clear SS
-    assertPositiveFloat(tailTolerence)
+    WecOptLib.errorCheck.assertPositiveFloat(tailTolerence)
         
     % Remove tails of the spectra; return indicies of the vals>tol% of max
     specGreaterThanTolerence = find(S > max(S)*tailTolerence/100);
@@ -110,204 +109,9 @@ function [noTailW, noTailS] = removeTails(w, S, tailTolerence)
     iEnd   = max(specGreaterThanTolerence);
     iSkip  = 1;
     
-    assertGreaterThanOrEqual(iEnd, iStart)
+    WecOptLib.errorCheck.assertGreaterThanOrEqual(iEnd, iStart)
 
     noTailW = w(iStart:iSkip:iEnd);
     noTailS = S(iStart:iSkip:iEnd);    
 
-end
-
-
-function [] = assertPositiveFloat(x)
-    msgPos = [sprintf('%f must be a positive',x)];
-    msgFloat = [sprintf('%f must be a float ',x)];
-    ID = 'WecOptLib:utilityFailure:positiveFloat';   
-    try
-        assert(all(x>=0),ID,msgPos);
-    catch ME
-        throw(ME)
-    end
-    try
-        assert(isa(x,'numeric'),ID,msgFloat);
-    catch ME
-        throw(ME)
-    end
-end
-
-
-function [] = assertPositiveInt(x)
-    msgInt = [sprintf('%f must be an Integer', x)];
-    msgPos = [sprintf('%f must be positive ', x)];
-    ID = 'WecOptLib:utilityFailure:positiveInt';      
-    try        
-        assert(all(x>=0),ID,msgPos);
-    catch ME
-        throw(ME)
-    end
-    
-    try
-        % isa(x,'integer') doen not work for x=1 default specification ;
-        assert(all(mod(x,1)==0),ID,msgInt);
-    catch ME
-        throw(ME)
-    end    
-end
-
-
-function [] = assertMinBinsGreaterThanOne(minBins)
-    msg = ['minBin values must be greater than 1'];
-    ID = 'WecOptLib:utilityFailure:minBinsGreaterThanOne';
-    try
-        assert(all(minBins>1),ID,msg);
-    catch ME
-        throw(ME)
-    end
-end
-
-
-function [] = assertBinsGreaterThanMinBins(SS,minBins)
-    lenMinBins = length(minBins);
-    if lenMinBins ==1
-        msg = ['Length of spectra must be greater than or equal to the'...
-               'specified minBins'];
-        ID = 'WecOptLib:utilityFailure:SSBinsLessThanSingleMinBins';
-        try
-            for i=1:length(SS)    
-                SBins = length(SS(i).S);
-                assert(SBins>=minBins,ID,msg);
-            end
-        catch ME
-            throw(ME)
-        end
-        
-    elseif lenMinBins == length(SS)
-        msg = ['Each minBin must be greater than or equal to the'...
-               'associated Spectra'];
-        ID = 'WecOptLib:utilityFailure:SSBinsLessThanMultiMinBins';
-        try
-            for i=1:length(SS)    
-                minBin = minBins(i);
-                SBins = length(SS(i).S);
-                assert(SBins>=minBin,ID,msg);
-            end
-        catch ME
-            throw(ME)
-        end
-    end
-end
-
-
-function [] = assertLengthOneOrLengthSS(x,SS)
-    msg = 'x must be of length 1 or length of SS';
-    ID = 'WecOptLib:utilityFailure:incorrectLength';
-    try
-        lenX = length(x);
-        lenSS = length(SS);    
-        assert(or(lenX==1, lenX==lenSS),ID,msg);
-    catch ME
-        throw(ME)
-    end
-end
-        
-
-function [] = assertGreaterThanOrEqual(x, y)
-    msg = ['x must be greater than or equal to y'];
-    ID = 'WecOptLib:utilityFailure:greaterThanOrEqual';
-    try
-        assert(x>=y,ID,msg) ;
-    catch ME
-        throw(ME)
-    end
-end
-
-
-function [] = assertEqualLength(x, y)
-    msg = ['x and y must be of equal length'];
-    ID = 'WecOptLib:utilityFailure:unequalLengths';
-    try
-        assert(length(x)==length(y),ID,msg);
-    catch ME
-        throw(ME)
-    end
-end
-
-function plotNoTailsSS(originalSS, noTailsSS)
-    % Creates plots for each SS comparing the orignal SS to the downSampled
-    % SS
-    %
-    % Parameters
-    % ----------
-    % originalSS: struct
-    %    The original spectrum
-    % noTailsSS: struct
-    %    The spectrum with the tails removed
-    %
-    % Returns
-    % -------
-    % plots comparing each sea-state
-            
-    WecOptLib.utils.checkSpectrum(originalSS);
-    WecOptLib.utils.checkSpectrum(noTailsSS);
-    assertEqualLength(originalSS, noTailsSS)
-    
-    figureN = get(gcf,'Number');
-    
-    for i=1:length(originalSS)
-        figure(figureN + i)
-        hold on;
-        
-        wOrig = originalSS(i).w;
-        SOrig = originalSS(i).S;
-        wMod = noTailsSS(i).w;
-        SMod = noTailsSS(i).S;
-        
-        markerOrig = '-';
-        markerMod  = 'o';
-        
-        labelOrig= sprintf('Original (N=%d)' ,length(wOrig));
-        labelMod = sprintf('Removed Tails (N=%d)',length(wMod));
-        
-        plotWVersusS(wOrig, SOrig, markerOrig, labelOrig)
-        plotWVersusS(wMod,  SMod,  markerMod, labelMod)
-        
-        
-        tailCutIn = min(wMod);
-        tailCutOut= max(wMod);
-        cutIn = [tailCutIn tailCutIn];
-        cutOut = [tailCutOut tailCutOut];
-        
-        yMin=0;
-        yMax=12;
-                        
-        plotWVersusS(cutIn, [yMin yMax], '--', 'Cut In')
-        plotWVersusS(cutOut,[yMin yMax], '-.', 'Cut Out')
-        
-        legend()
-        xlim([ 0  4])
-        ylim([ yMin yMax])
-        xlabel('Frequency [$\omega$]','Interpreter','latex')
-        ylabel('Spectral Density','Interpreter','latex')
-        grid                        
-        
-    end
-        
-    
-end
-
-function plotWVersusS(w, S, marker, label)
-    % Plots angular frequency versus Spectral Density    
-    %
-    % Parameters
-    % ----------
-    % w: vector
-    %    Angular frequencies
-    % S: vector
-    %    Spectral Densities
-    % marker: string
-    %    marker used to plt
-    % label: String
-    %    legend label
-    
-    plot(w, S, marker, 'DisplayName', label)           
-    
 end
