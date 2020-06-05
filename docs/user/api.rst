77394fd9a12938d28a18785111e7f5e633a9cc8a
Mathew Topper <damm_horse@yahoo.co.uk>
2020-06-05 18:10:38 +0100

    Begin updating inline docs
    Added option of hiding MATLAB docs commands from Sphinx by placing them
    between "--" markers.

--- docs/user/api.rst
+++ docs/user/api.rst
@@ -2,41 +2,15 @@
 API
 ***
 
-Experimental
-============
-
-WecOptLib.experimental
-----------------------
-
-.. mat:automodule:: +WecOptLib.+experimental
-    :members:
-
-WecOptLib.experimental.callbacks.geometry
------------------------------------------
-
-.. mat:automodule:: +WecOptLib.+experimental.+callbacks.+geometry
-    :members:
-
 WecOptTool
 ==========
 
 The WecOptTool package provides the main interface for the toolbox.
 
-.. mat:automodule:: +WecOptTool
-    :members:
-
-WecOptTool.control
+WecOptTool.base
 ------------------
 
 The control subpackage provides options for device control.
 
-.. mat:automodule:: +WecOptTool.+control
-    :members:
-
-WecOptTool.geom
----------------
-
-The geom subpackage provides options for device geometry variation.
-
-.. mat:automodule:: +WecOptTool.+geom
+.. mat:automodule:: +WecOptTool.+base
     :members:
--- toolbox/+WecOptTool/+base/Data.m
+++ toolbox/+WecOptTool/+base/Data.m
@@ -1,4 +1,41 @@
 classdef (Abstract) Data < dynamicprops
+    % Abstract class for creating new types.
+    %
+    % WecOptLib types act like structs with added required fields and
+    % validation of those fields (in the input stuct). The abstract 
+    % struct property ``meta`` must be implemented in concrete classes,
+    % with fields "name" and "validation", for example::
+    %
+    %    meta = struct("name", {"Var1",    ...
+    %                           "Var2"},   ...
+    %                  "validation", {@isnumeric,    ...
+    %                                 @isstring});
+    %
+    % --
+    %
+    % See also WecOptTool.types
+    %
+    % --
+    
+    % Copyright 2020 National Technology & Engineering Solutions of Sandia, 
+    % LLC (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the 
+    % U.S. Government retains certain rights in this software.
+    %
+    % This file is part of WecOptTool.
+    % 
+    %     WecOptTool is free software: you can redistribute it and/or 
+    %     modify it under the terms of the GNU General Public License as 
+    %     published by the Free Software Foundation, either version 3 of 
+    %     the License, or (at your option) any later version.
+    % 
+    %     WecOptTool is distributed in the hope that it will be useful,
+    %     but WITHOUT ANY WARRANTY; without even the implied warranty of
+    %     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    %     GNU General Public License for more details.
+    % 
+    %     You should have received a copy of the GNU General Public 
+    %     License along with WecOptTool.  If not, see 
+    %     <https://www.gnu.org/licenses/>.
     
     properties (Abstract, GetAccess=protected)
         meta
