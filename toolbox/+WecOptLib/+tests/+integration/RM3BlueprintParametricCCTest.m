fc5cd8b98c70f9f146c41de6d131030d3a7eb753
Mathew Topper <damm_horse@yahoo.co.uk>
2020-07-01 18:39:35 +0100

    Remove subSampleFreqs function

--- toolbox/+WecOptLib/+tests/+integration/RM3BlueprintParametricCCTest.m
+++ toolbox/+WecOptLib/+tests/+integration/RM3BlueprintParametricCCTest.m
@@ -19,8 +19,9 @@ classdef RM3BlueprintParametricCCTest < matlab.unittest.TestCase
             
             S = WecOptLib.tests.data.exampleSpectrum();
             S.ph = rand(length(S.w),1)* 2 * pi;
-            [S.w, S.S] = WecOptLib.utils.subSampleFreqs(S);
-            testCase.SS = WecOptTool.types("SeaState", S);
+            testCase.SS = WecOptTool.types("SeaState", S,           ...
+                                           "extendFrequencies", 2,  ...
+                                           "resampleByStep", 0.05);
             w = testCase.SS.getRegularFrequencies(0.5);
 
             testCase.blueprint = RM3();
@@ -62,7 +63,7 @@ classdef RM3BlueprintParametricCCTest < matlab.unittest.TestCase
 
         function test_runParametric(testCase)
             
-            expSol = 4.714404025520907e+06;
+            expSol = 4.759798816032207e+06;
             verifyEqual(testCase,                           ...
                         testCase.device.aggregation.pow,    ...
                         expSol,                             ...
