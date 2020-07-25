26234dc08cd671584da3d744dcf14b765685e97b
Mathew Topper <damm_horse@yahoo.co.uk>
2020-07-25 18:48:23 +0100

    Undelete plotFreq code

--- toolbox/+WecOptTool/+types/Performance.m
+++ toolbox/+WecOptTool/+types/Performance.m
@@ -128,7 +128,52 @@ classdef Performance < handle
         
         function plotFreq(obj,fig)
             
+            if nargin < 2 || isempty(fig)
+                fig = figure;
+            end
+            set(fig,'Name','Performance.plotFreq');
             
+            fns = {'F0','u','Fpto'};
+            mrks = {'o','.','+','s'};
+            
+            n = length(obj);
+            for jj = 1:n
+                for ii = 1:length(fns)
+                    
+                    fv = obj(jj).(fns{ii})(:,1); % use the first column if this is PS
+                    
+                    % mag plot
+                    ax(jj,1) = subplot(2,n,sub2ind([n,2],jj,1));
+                    title(obj(jj).name,'interpreter','none')
+                    hold on
+                    grid on
+                    
+                    stem(ax(jj,1),obj(jj).w, mag2db(abs(fv))...
+                        ,mrks{ii},...
+                        'DisplayName',fns{ii},...
+                        'MarkerSize',8,...
+                        'Color','b')
+                    
+                    % phase plot
+                    ax(jj,2) = subplot(2,n,sub2ind([n,2],jj,2));
+                    hold on
+                    grid on
+                    
+                    stem(ax(jj,2),obj(jj).w, angle(fv)...
+                        ,mrks{ii},...
+                        'DisplayName',fns{ii},...
+                        'MarkerSize',8,...
+                        'Color','b')
+                    
+                    ylim(ax(jj,2),[-pi,pi])
+                end
+                xlabel(ax(jj,2),'Frequency [rad/s]')
+            end
+            ylabel(ax(1,1),'Magnitude [dB]')
+            ylabel(ax(1,2),'Angle [rad]')
+            legend(ax(n,1))
+            linkaxes(ax,'x')
+            linkaxes(ax(:,1),'y')
             
         end
         
