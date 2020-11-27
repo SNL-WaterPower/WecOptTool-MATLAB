bc3de99e3d165d7e9356f08a21437343d5d5ee62
Mathew Topper <damm_horse@yahoo.co.uk>
2020-11-27 12:49:37 +0000

    Make sure everything has license boilerplate

--- examples/Flapper/Flapper_caseA.m
+++ examples/Flapper/Flapper_caseA.m
@@ -50,3 +50,22 @@ I = mass / 12 * (4 * height ^ 2 + length ^ 2);
 % Print and plot comparison
 performance.summary()
 performance.plotTime()
+
+% Copyright 2020 National Technology & Engineering Solutions of Sandia, 
+% LLC (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the 
+% U.S. Government retains certain rights in this software.
+%
+% This file is part of WecOptTool.
+% 
+%     WecOptTool is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     WecOptTool is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with WecOptTool.  If not, see <https://www.gnu.org/licenses/>.
--- examples/Flapper/simulateDevice.m
+++ examples/Flapper/simulateDevice.m
@@ -25,6 +25,25 @@ function [performance, dynModel] = simulateDevice(I,            ...
     %
     % See also WecOptTool.SeaState, interp1
     
+    % Copyright 2020 National Technology & Engineering Solutions of Sandia, 
+    % LLC (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the 
+    % U.S. Government retains certain rights in this software.
+    %
+    % This file is part of WecOptTool.
+    % 
+    %     WecOptTool is free software: you can redistribute it and/or modify
+    %     it under the terms of the GNU General Public License as published by
+    %     the Free Software Foundation, either version 3 of the License, or
+    %     (at your option) any later version.
+    % 
+    %     WecOptTool is distributed in the hope that it will be useful,
+    %     but WITHOUT ANY WARRANTY; without even the implied warranty of
+    %     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    %     GNU General Public License for more details.
+    % 
+    %     You should have received a copy of the GNU General Public License
+    %     along with WecOptTool.  If not, see <https://www.gnu.org/licenses/>.
+        
     arguments
         I (1, 1) double
         hydro (1,1) WecOptTool.Hydrodynamics
@@ -423,22 +442,3 @@ function [powTot, fRes, tRes] = getPSPhasePower(dynModel, ph)
         P = X(1:end-2)' * dynModel.H_mat * X(1:end-2); % 1/2 factor dropped for simplicity
     end
 end
-
-% Copyright 2020 National Technology & Engineering Solutions of Sandia, 
-% LLC (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the 
-% U.S. Government retains certain rights in this software.
-%
-% This file is part of WecOptTool.
-% 
-%     WecOptTool is free software: you can redistribute it and/or modify
-%     it under the terms of the GNU General Public License as published by
-%     the Free Software Foundation, either version 3 of the License, or
-%     (at your option) any later version.
-% 
-%     WecOptTool is distributed in the hope that it will be useful,
-%     but WITHOUT ANY WARRANTY; without even the implied warranty of
-%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%     GNU General Public License for more details.
-% 
-%     You should have received a copy of the GNU General Public License
-%     along with WecOptTool.  If not, see <https://www.gnu.org/licenses/>.
